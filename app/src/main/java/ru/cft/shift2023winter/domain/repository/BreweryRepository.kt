b176420f8518d1c1677db0750418cd90d0d7f7f1
Ivan <Streafyhs@gmail.com>
2023-02-02 22:11:15 +0700

    Поменял возвращаемое значение в репозитории и юзкейсе

--- app/src/main/java/ru/cft/shift2023winter/domain/repository/BreweryRepository.kt
+++ app/src/main/java/ru/cft/shift2023winter/domain/repository/BreweryRepository.kt
@@ -1,12 +1,14 @@
 package ru.cft.shift2023winter.domain.repository
 
+import androidx.paging.PagingData
+import kotlinx.coroutines.flow.Flow
 import ru.cft.shift2023winter.domain.entity.Brewery
 
 interface BreweryRepository {
 
     suspend fun getList(): List<Brewery>
 
-    suspend fun getPage(pageNumber: Int): List<Brewery>
+    fun getPage(): Flow<PagingData<Brewery>>
 
     suspend fun getById(id: String): Brewery
 }
--- app/src/main/java/ru/cft/shift2023winter/domain/use_case/GetBreweriesPageUseCase.kt
+++ app/src/main/java/ru/cft/shift2023winter/domain/use_case/GetBreweriesPageUseCase.kt
@@ -1,5 +1,7 @@
 package ru.cft.shift2023winter.domain.use_case
 
+import androidx.paging.PagingData
+import kotlinx.coroutines.flow.Flow
 import ru.cft.shift2023winter.domain.entity.Brewery
 import ru.cft.shift2023winter.domain.repository.BreweryRepository
 import javax.inject.Inject
@@ -7,6 +9,6 @@ import javax.inject.Inject
 class GetBreweriesPageUseCase @Inject constructor(
     private val repository: BreweryRepository
 ) {
-    suspend operator fun invoke(pageNumber: Int): List<Brewery> =
-        repository.getPage(pageNumber)
+    operator fun invoke(pageNumber: Int): Flow<PagingData<Brewery>> =
+        repository.getPage()
 }
